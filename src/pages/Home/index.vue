977980691da44ea1d8c693728a2720aff38342c6
XTZJ-2020UKEMRC\Administrator <d1005yt@163.com>
2020-12-25 16:52:54 +0800

    动画

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,14 +1,35 @@
 <template>
-    <div>
-        首页
+    <div style="width:100%;height:100%;">
+        <div class="title">
+           
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: '',
+    data(){
+        return{
+            left:0,
+        }
+    },
+    mounted(){
+        //this.changeLeft()
+    },
+    methods:{
+        changeLeft(){
+            let left=setInterval(()=>{
+                if(this.left>1000){
+                    this.left=0
+                }
+                this.left++
+            },)
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
+
 </style>
